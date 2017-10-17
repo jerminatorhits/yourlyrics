ac1397fb44c3d64cd9dee8f9e8ce485f96d9366c
Jeremy Kwa <jkwa@ucsd.edu>
2017-10-17 11:23:59 -0700

    Added 700,000 songs

--- db/schema.rb
+++ db/schema.rb
@@ -10,54 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170202020351) do
+ActiveRecord::Schema.define(version: 20170131220247) do
 
   create_table "ciphers", id: false, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
-    t.integer "bag_id",                    null: false
-    t.string  "word",           limit: 13
-    t.string  "word_unstemmed", limit: 15
+    t.integer "bag_id",            null: false
+    t.string  "word",   limit: 13
     t.index ["bag_id"], name: "bag_id", unique: true, using: :btree
   end
 
-  create_table "codes", id: false, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
-    t.integer "bag_id",                    default: 0, null: false
-    t.string  "word_unstemmed", limit: 15
-  end
-
-  create_table "details", id: false, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
-    t.string "tid",         limit: 18
-    t.string "artist",      limit: 50
-    t.string "title",       limit: 150
-    t.string "mxm_tid",     limit: 7
-    t.string "artist_name", limit: 50
-    t.string "title_name",  limit: 50
-  end
-
-  create_table "song1s", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
-    t.string   "tid"
-    t.string   "mxmid"
-    t.string   "lyrics_hash"
-    t.string   "artist"
-    t.string   "title"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
-
-  create_table "songs", primary_key: "tid", id: :string, limit: 18, default: "", collation: "utf8_general_ci", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
-    t.string "mxmid",       limit: 7,   collation: "utf8_general_ci"
-    t.string "lyrics_hash", limit: 800, collation: "utf8_general_ci"
-    t.string "artist",      limit: 50,  collation: "utf8_general_ci"
-    t.string "title",       limit: 150, collation: "utf8_general_ci"
-  end
-
-  create_table "songzers", primary_key: "tid", id: :string, limit: 18, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
+  create_table "extra", primary_key: "tid", id: :string, limit: 18, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.string "mxmid",       limit: 7
     t.string "lyrics_hash", limit: 800
   end
 
-  create_table "stems", id: false, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
-    t.string "word",           limit: 15
-    t.string "word_unstemmed", limit: 15
+  create_table "songs", id: false, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
+    t.string "tid",    limit: 18,  collation: "utf8_general_ci"
+    t.string "mxmid",  limit: 7,   collation: "utf8_general_ci"
+    t.string "artist", limit: 50,  collation: "utf8_general_ci"
+    t.string "title",  limit: 150, collation: "utf8_general_ci"
   end
 
 end
